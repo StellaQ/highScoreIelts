8dea6f7608c5dbaa7de54943ffb23b59a155b9b5
StellaQ <845521178@qq.com>
2025-04-10 10:29:19 +0800

    把代码中的无用代码清理掉

--- miniprogram/miniprogram/assets/data/userConfig.js
+++
@@ -1,6 +0,0 @@
-const userConfig = {
-  basicNumberOfUsesEachDay: 20,  //默认非vip用户每天最basic能刷的题目数量
-  gapNumberOfUsesByNew: 20, //默认非vip用户每拉新一个就能获得的刷题目的次数，次数没有使用时间限制
-  inviteNumberToBeVip: 50 //默认非vip用户需要拉新的人数才能转为vip
-};
-module.exports = userConfig;
