f5533ff9f8577f3f4e693e453ef20e05b33bbaa2
StellaQ <845521178@qq.com>
2025-05-23 14:14:16 +0800

    test线上修改配置

--- config/configForMiniProgram.js
+++ config/configForMiniProgram.js
@@ -1,19 +1,19 @@
 module.exports = {
     SIGN_IN_POINTS: 10, // 有用：每次签到获得的积分
     INVITE_POINTS: 20,  // 有用：邀请奖励积分
-    AI_PART1_POINTS: 10, // 有用：调用AI扣分数
-    AI_PART2_POINTS: 20, // 有用：调用AI扣分数
-    AI_PART3_POINTS: 10, // 有用：调用AI扣分数
+    AI_PART1_POINTS: 5, // 有用：调用AI扣分数
+    AI_PART2_POINTS: 10, // 有用：调用AI扣分数
+    AI_PART3_POINTS: 5, // 有用：调用AI扣分数
     // 会员订阅价格配置（单位：元）
     VIP_SEASON_PRICE: 1,  // 季卡价格
     VIP_YEARLY_PRICE: 198,  // 年卡价格
     VIP_SEASON_NAME: '季卡',  // 季卡名称
     VIP_YEARLY_NAME: '年卡',  // 年卡名称
     // 题目批次配置
-    CURRENT_BATCH: 'B0',  // 当前批次，如 B1, B2, B3...
-    PREVIOUS_BATCH: '2025Q1',  // 上一批次
+    CURRENT_BATCH: '2025Q2',  // 当前批次，如 B1, B2, B3...
+    PREVIOUS_BATCH: 'B0',  // 上一批次
     // 题库更新时间
-    QUESTION_BANK_UPDATE_DATE: '2025-04-15',
+    QUESTION_BANK_UPDATE_DATE: '2025-05-23',
     showUpdateAlert: true,  // 有用：是否显示更新提示
     updateTip: "5月-8月" // 有用：更新提示内容
   };
